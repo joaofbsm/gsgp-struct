1252aded8e102dc30531085f99e29837a3addca4
Joao Martins <joaofbsm@hotmail.com>
2017-12-01 00:54:48 -0200

    Fix GP execute script
     - It was necessary to use the number of attributes in a dataset(+5) to set
    a parameter.

--- execute_gp.sh
+++ execute_gp.sh
@@ -8,6 +8,7 @@
 #==============================================================================
 
 datasets=("airfoil" "ccn" "ccun" "concrete" "energyCooling" "energyHeating" "parkinsons" "ppb-wth0s" "towerData" "wineRed" "wineWhite" "yacht")
+num_input_attrib=("10" "127" "129" "13" "13" "13" "23" "259" "30" "16" "16" "11")
 gp_path=$(pwd)"/gp"
 datasets_path=$(pwd)"/datasets/original"
 results_path=$(pwd)"/results/gp"
@@ -15,10 +16,10 @@ scripts_path=$(pwd)"/scripts"
 
 mkdir -p "$results_path"
 
-for dataset in "${datasets[@]}"
+for ((i=0; i<"${#datasets[@]}"; i++));
 do
     echo "Executing $dataset"
-    java -Xms512m -Xmx8g -cp "$gp_path"/GP.jar ec.gp.core.Experimenter -o "$results_path"/out_files -t 4 -n 1 -B -p "$gp_path"/parameters/gp-with-AQ.params -i "$datasets_path"/"$dataset"-#.csv -a output-"$dataset" -g 1 -P generations=250 -P pop.subpop.0.size=1000 -P select.tournament.size=10 -P gp.fs.0.size=13 -nf 1 -s 123456 > "$results_path"/"$dataset".txt
+    java -Xms512m -Xmx8g -cp "$gp_path"/GP.jar ec.gp.core.Experimenter -o "$results_path"/out_files -t 4 -n 1 -B -p "$gp_path"/parameters/gp-with-AQ.params -i "$datasets_path"/"${datasets[$i]}"-#.csv -a output-"${datasets[$i]}" -g 1 -P generations=250 -P pop.subpop.0.size=1000 -P select.tournament.size=7 -P gp.fs.0.size="${num_input_attrib[$i]}" -nf 1 -s 123456 > "$results_path"/"${datasets[$i]}".txt
 done
 
 python3 "$scripts_path"/combine_gp_out.py
